724867abbec9af85c4a80dde11c3942ba147be19
n4o847 <22975590+n4o847@users.noreply.github.com>
2018-11-15 00:39:03 +0900

    Implement a simple counter

--- src/Main.elm
+++ src/Main.elm
@@ -1,4 +1,46 @@
+import Browser
 import Html exposing (..)
+import Html.Events exposing (..)
 
+
+main : Program () Model Msg
 main =
-    text "Hello, world!"
+    Browser.sandbox
+        { init = init
+        , view = view
+        , update = update
+        }
+
+
+type alias Model =
+    Int
+
+
+init : Model
+init =
+    0
+
+
+type Msg
+    = Increment
+    | Decrement
+
+
+update : Msg -> Model -> Model
+update msg model =
+    case msg of
+        Increment ->
+            model + 1
+
+        Decrement ->
+            model - 1
+
+
+view : Model -> Html Msg
+view model =
+    div []
+        [ h1 [] [ text "Hello" ]
+        , div [] [ text (String.fromInt model) ]
+        , button [ onClick Decrement ] [ text "-" ]
+        , button [ onClick Increment ] [ text "+" ]
+        ]
