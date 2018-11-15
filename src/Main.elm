8e72287ad2938cb78b9536273920a91931a0074c
n4o847 <22975590+n4o847@users.noreply.github.com>
2018-11-15 23:18:29 +0900

    Display board

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,8 @@ import Browser
 import Html exposing (..)
 import Html.Events exposing (..)
 
+import Array exposing (Array)
+
 
 main : Program () Model Msg
 main =
@@ -12,13 +14,34 @@ main =
         }
 
 
+
+-- MODEL
+
+
 type alias Model =
-    Int
+    { board : Board
+    }
+
+type alias Board =
+    Array Row
+
+type alias Row =
+    Array Cell
+
+type Cell
+    = Tile Int
+    | Empty
 
 
 init : Model
 init =
-    0
+    { board =
+        Array.repeat 4 <| Array.repeat 4 <| Tile 2
+    }
+
+
+
+-- UPDATE
 
 
 type Msg
@@ -30,17 +53,43 @@ update : Msg -> Model -> Model
 update msg model =
     case msg of
         Increment ->
-            model + 1
+            model
 
         Decrement ->
-            model - 1
+            model
+
+
+
+-- VIEW
 
 
 view : Model -> Html Msg
 view model =
     div []
         [ h1 [] [ text "Hello" ]
-        , div [] [ text (String.fromInt model) ]
-        , button [ onClick Decrement ] [ text "-" ]
-        , button [ onClick Increment ] [ text "+" ]
+        , viewBoard model.board
         ]
+
+
+viewBoard : Board -> Html Msg
+viewBoard board =
+    div []
+        <| Array.toList (Array.map viewRow board)
+
+
+viewRow : Row -> Html Msg
+viewRow row =
+    div []
+        <| Array.toList (Array.map viewCell row)
+
+
+viewCell : Cell -> Html Msg
+viewCell cell =
+    case cell of
+        Tile number ->
+            span []
+            [ text (String.fromInt number)
+            ]
+
+        Empty ->
+            span [] []
